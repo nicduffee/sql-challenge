df00800122f209ed734c575a984e45d8ae187263
nicduffee <nicduffee@hotmail.com>
2023-07-20 09:39:01 -0700

    Data Engineering Completed. Table Schema created, Data imported using PGadmin

---
+++ EmployeeSQL/Tables Schema.sql
@@ -0,0 +1,71 @@
+﻿CREATE TABLE "TITLES" (
+    "title_id" VARCHAR(50)   NOT NULL,
+    "title" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_TITLES" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "EMPLOYEES" (
+    "emp_no" INT   NOT NULL,
+    "emp_title" VARCHAR(50)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(200)   NOT NULL,
+    "last_name" VARCHAR(200)   NOT NULL,
+    "sex" VARCHAR(20)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_EMPLOYEES" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "SALARIES" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_SALARIES" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "DEPARTMENTS" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "dept_name" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_DEPARTMENTS" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "DEPT_MANAGER" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_DEPT_MANAGER" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "DEPT_EMP" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_DEPT_EMP" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+ALTER TABLE "EMPLOYEES" ADD CONSTRAINT "fk_EMPLOYEES_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "TITLES" ("title_id");
+
+ALTER TABLE "SALARIES" ADD CONSTRAINT "fk_SALARIES_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "EMPLOYEES" ("emp_no");
+
+ALTER TABLE "DEPT_MANAGER" ADD CONSTRAINT "fk_DEPT_MANAGER_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "DEPARTMENTS" ("dept_no");
+
+ALTER TABLE "DEPT_MANAGER" ADD CONSTRAINT "fk_DEPT_MANAGER_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "EMPLOYEES" ("emp_no");
+
+ALTER TABLE "DEPT_EMP" ADD CONSTRAINT "fk_DEPT_EMP_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "EMPLOYEES" ("emp_no");
+
+ALTER TABLE "DEPT_EMP" ADD CONSTRAINT "fk_DEPT_EMP_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "DEPARTMENTS" ("dept_no");
+
